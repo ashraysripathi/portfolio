c239deaa8c4ecaeea63010370f235c2f10cac74c
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2020-03-10 14:48:59 +0530

    Added Home PAge

---
+++ src/components/homeComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./style.css";
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div class="loop1" id="home">
+        <p class="headtext" id="one">
+          The eternal silence of these infinite spaces frightens me.
+          <br /> Blaise Pascal
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
