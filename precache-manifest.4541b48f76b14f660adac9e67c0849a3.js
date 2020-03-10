51250f4e8931cc424ebc939e8022e62201bd9a35
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2020-03-10 14:50:18 +0530

    Updates

---
+++ precache-manifest.4541b48f76b14f660adac9e67c0849a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b1eb238ffc2e220320530a055dd8d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7b2a59eb716ac07eb871",
+    "url": "/portfolio/static/css/main.b44200b3.chunk.css"
+  },
+  {
+    "revision": "ad384d14fcd50470709f",
+    "url": "/portfolio/static/js/2.d5ecb53c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.d5ecb53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2a59eb716ac07eb871",
+    "url": "/portfolio/static/js/main.7a8f8211.chunk.js"
+  },
+  {
+    "revision": "bf88e1f0e454a9e9cb8a",
+    "url": "/portfolio/static/js/runtime-main.14dffd18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.fe5e0f054e26f4716ed2620af9d0dc22.js"
+  "/portfolio/precache-manifest.4541b48f76b14f660adac9e67c0849a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
