0c63a1f2d88cb84d4c56db6638cd8c7d0c5c56b1
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2020-03-10 15:00:39 +0530

    added desc

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -4,11 +4,16 @@ class Home extends Component {
   state = {};
   render() {
     return (
-      <div class="loop1" id="home">
-        <p class="headtext" id="one">
-          The eternal silence of these infinite spaces frightens me.
-          <br /> Blaise Pascal
-        </p>
+      <div>
+        <div class="loop1" id="home">
+          <p class="headtext" id="one">
+            The eternal silence of these infinite spaces frightens me.
+            <br /> Blaise Pascal
+          </p>
+        </div>
+        <div id="about" class="aboutloop">
+          <p>Just a student working towards a degree in Computer Science.</p>
+        </div>
       </div>
     );
   }
